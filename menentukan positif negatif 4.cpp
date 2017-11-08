8af9afd24a4c95030d755e83647ba0c5802b0bdd
MuhammadRosyad <achaibuy01@gmail.com>
2017-11-08 19:13:50 +0800

    menentukan apakah bilangan tersebut positif, negatif, atau nol

---
+++ menentukan positif negatif 4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	int nilai;
+	
+	cout<<"masukkan nilai :";
+	cin>>nilai;
+	
+	if(nilai>0)
+	cout<<"nilai positif";
+	else if(nilai<0)
+	cout<<"nilai negatif";
+	else
+	cout<<"nilai 0";
+}
+
